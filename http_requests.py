d2986f6307d908be442246f876da39e93ab58923
FastDevelopmentP <pete5fast@gmail.com>
2025-10-26 21:18:29 -0400

    7 tests passing

--- http_requests.py
+++ http_requests.py
@@ -16,8 +16,8 @@ class JSONPlaceholder:
         else:
             return None
         
-    def post_request (self, data: dict):
-        response = requests.post(self.base_url, json=data)
+    def post_request(self, data):
+        response = requests.post(self.base_url, data=data)
         
         if response.status_code == 201:
             return{
@@ -27,9 +27,38 @@ class JSONPlaceholder:
             }
         else:
             return None
-        
     
+    def update_user(self, userId, title, body,):
+        payload = {
+            "title": title,
+            "body": body,
+        }   
+        
+        response = requests.put(f"{self.base_url}/{userId}", payload)
+
+        if response.status_code == 200:
+            return{
+                "status_code": response.status_code,
+                "headers": response.headers,
+                "content": response.content[:500],
+            }
+        else:
+            return None
 
 
+    def delete_user(self, userId, title, body,):
+        payload = {
+            "title": title,
+            "body": body,
+        }   
+        
+        response = requests.delete(f"{self.base_url}/{userId}", payload)
 
-
+        if response.status_code == 200:
+            return{
+                "status_code": response.status_code,
+                "headers": response.headers,
+                "content": response.content[:500],
+            }
+        else:
+            return None
